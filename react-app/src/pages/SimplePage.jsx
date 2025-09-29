eacfedd65515665a1b18a65c97bf3d726017df7e
pb-06 <kkandomail@gmail.com>
2025-09-29 08:35:03 +0200

    Catch submit event

--- react-app/src/pages/SimplePage.jsx
+++ react-app/src/pages/SimplePage.jsx
@@ -1,11 +1,16 @@
 /* SimplePage.jsx */
 import React from "react";
 export default class SimplePage extends React.Component {
+    handleFormSubmit = e => {
+        e.preventDefault();
+        console.log("handleFormSubmit e", e);
+    }
+    
     render() {
         return (
             <div id="content-simple" className="tab-content active">
             <h2>Simple Email Form</h2>
-                <form>
+                <form onSubmit={this.handleFormSubmit}>
                     <label htmlFor="simpleEmail">Email address:</label>
                     <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on" />
                     <button type="submit">Submit</button>
