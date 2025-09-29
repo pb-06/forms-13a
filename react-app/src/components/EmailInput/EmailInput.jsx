87dc49aa5634a2608ed20bf9fed9abda02d3ec85
pb-06 <kkandomail@gmail.com>
2025-09-29 08:53:19 +0200

    Outsource to EmailInput

---
+++ react-app/src/components/EmailInput/EmailInput.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default class EmailInput extends React.Component {
+    state = {
+        email: ''
+    }
+
+    setEmail = function (email) {
+        console.log('setEmail email', email);
+        this.setState({ email });
+    }
+
+    render() {
+        return (
+            <>
+                <label htmlFor="simpleEmail">Email address:</label>
+                <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on"
+                    onChange={e => this.setEmail(e.target?.value)}
+                    onBlur={this.handleEmailBlur}
+                />
+            </>
+        )
+    }
+}
--- react-app/src/pages/SimplePage.jsx
+++ react-app/src/pages/SimplePage.jsx
@@ -1,29 +1,29 @@
 /* SimplePage.jsx */
 import React from "react";
+import EmailInput from "../components/EmailInput/EmailInput";
+
 export default class SimplePage extends React.Component {
     state = {
         email: ''
     }
 
-    setEmail = function(email) {
-        console.log('setEmail email', email);
-        this.setState({email});
-    }
-    
     handleFormSubmit = e => {
         e.preventDefault();
-        
+
         //console.log("handleFormSubmit e", e);
         console.log('email', this.state.email);
     }
-    
+
+    handleEmailBlur = e => {
+        console.log('handleEmailBlur e', e);
+    }
+
     render() {
         return (
             <div id="content-simple" className="tab-content active">
-            <h2>Simple Email Form</h2>
+                <h2>Simple Email Form</h2>
                 <form onSubmit={this.handleFormSubmit}>
-                    <label htmlFor="simpleEmail">Email address:</label>
-                    <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on" onChange={e=>this.setEmail(e.target?.value)} />
+                    <EmailInput />
                     <button type="submit">Submit</button>
                     <input type="reset" value={"Reset"} />
                 </form>
