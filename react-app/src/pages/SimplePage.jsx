276c47ac5123d95121f799e0efeef3f665442809
pb-06 <kkandomail@gmail.com>
2025-09-29 08:40:54 +0200

    Handle email change

--- react-app/src/pages/SimplePage.jsx
+++ react-app/src/pages/SimplePage.jsx
@@ -1,6 +1,15 @@
 /* SimplePage.jsx */
 import React from "react";
 export default class SimplePage extends React.Component {
+    state = {
+        email: ''
+    }
+
+    setEmail = function(email) {
+        console.log('setEmail email', email);
+        this.setState({email});
+    }
+    
     handleFormSubmit = e => {
         e.preventDefault();
         console.log("handleFormSubmit e", e);
@@ -12,7 +21,7 @@ export default class SimplePage extends React.Component {
             <h2>Simple Email Form</h2>
                 <form onSubmit={this.handleFormSubmit}>
                     <label htmlFor="simpleEmail">Email address:</label>
-                    <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on" />
+                    <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on" onChange={e=>this.setEmail(e.target?.value)} />
                     <button type="submit">Submit</button>
                     <input type="reset" value={"Reset"} />
                 </form>
